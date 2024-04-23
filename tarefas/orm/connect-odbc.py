6578ace5e03516da59e8632154b2b450d2f17b29
MarlonHD <marlonsilva840@gmail.com>
2024-04-22 22:29:55 -0300

    Questão 5 - letra A #1

--- tarefas/orm/connect-odbc.py
+++ tarefas/orm/connect-odbc.py
@@ -8,4 +8,13 @@ conn = psycopg2.connect(
             password=config('DATABASE_PASS')
         )
 
+
+cur = conn.cursor()
+
+#------------- A -------------
+atv = ("BD - Tarefa ODBC",3,"2024-04-21","2024-04-23")
+cur.execute("INSERT INTO atividade(descricao, projeto, data_inicio, data_fim) VALUES (%s, %s, %s, %s)", atv)
+conn.commit()
+
+cur.close()
 conn.close()
