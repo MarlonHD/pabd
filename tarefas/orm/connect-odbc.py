fee43a2099618647dee7efd33c21ef385f2b2a82
MarlonHD <marlonsilva840@gmail.com>
2024-04-22 22:30:20 -0300

    Questão 5 - letra B #1

--- tarefas/orm/connect-odbc.py
+++ tarefas/orm/connect-odbc.py
@@ -16,5 +16,10 @@ atv = ("BD - Tarefa ODBC",3,"2024-04-21","2024-04-23")
 cur.execute("INSERT INTO atividade(descricao, projeto, data_inicio, data_fim) VALUES (%s, %s, %s, %s)", atv)
 conn.commit()
 
+
+#------------- B -------------
+cur.execute("UPDATE projeto SET responsavel = %s WHERE codigo = %s", (14, 3))
+conn.commit
+
 cur.close()
 conn.close()
